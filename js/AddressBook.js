0df08b2d4ac132286e7603f8d14706a3e6146649
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-09-21 09:20:16 +0530

    feat:Ability To Create Address Book Contact

---
+++ js/AddressBook.js
@@ -0,0 +1,13 @@
+class AddressBook {
+
+    constructor(...params) {
+    this.firstName = params[0];
+    this.lastName = params[1];
+    this.address = params[2];
+    this.city = params[3];
+    this.state = params[4];
+    this.zipCode = params[5];
+    this.phoneNumber = params[6];
+    this.emailId = params[7];
+}
+}
